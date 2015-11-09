0ace1d34892f885cf01195fe762bbd191d56f1bf
Afonso F. Garcia <afonsogarcoia@icloud.com>
2015-11-09 12:59:55 +0000

    Refactor

--- funcoes.js
+++ funcoes.js
@@ -1,39 +1,48 @@
 var emFuncoes = true
 var caiu = false
+
 var text = ""
 var funcoes = ""
-function countup(yr,m,d,h,mn,s){
-  return differ_aux(yr+"-"+m+"-"+d+"T"+h+":"+mn+":"+s+"Z", moment().format())
-}
+
 function differ_aux(f,s){
   var two = moment(s)
   var one = moment(f)
+
   var diff = moment(two.diff(one))
   var difference = [diff.year()-1970, diff.month(), diff.date()-1, diff.hour(), diff.minute(), diff.second()]
+
   var text = difference[0] + " anos, "
   text += difference[1] + " meses, "
   text += difference[2] + " dias, "
   text += difference[3] + " horas, "
   text += difference[4] + " minutos e "
   text += difference[5] + " segundos."
+
   return text
 }
+
+function countup(yr,m,d,h,mn,s){
+  return differ_aux(yr+"-"+m+"-"+d+"T"+h+":"+mn+":"+s+"Z", moment().format())
+}
+
 function differ(fyr,fm,fd,fh,fmn,fs,syr,sm,sd,sh,smn,ss) {
   return differ_aux(fyr+"-"+fm+"-"+fd+"T"+fh+":"+fmn+":"+fs+"Z", syr+"-"+sm+"-"+sd+"T"+sh+":"+smn+":"+ss+"Z")
 }
+
+if(caiu)
+  funcoes = "SIM"
+else
+  funcoes = "N&Atilde;O"
+
 setInterval(function(){
-  if(emFuncoes) {
-    funcoes = "N&Atilde;O"
+  if(emFuncoes)
     text = "O governo est&aacute; em fun&ccedil;&otilde;es h&aacute; " + countup("2015","11","09","11","30","00")
-  } else {
-    if(caiu) {
+  else
+    if(caiu)
       text = "O governo esteve em fun&ccedil;&otilde;es durante " + differ("2015","11","09","11","30","00","2015","12","25","00","00","00")
-      funcoes = "SIM"
-    } else {
-      funcoes = "N&Atilde;O"
+    else
       text = "O governo ainda n&atilde;o est&aacute; em fun&ccedil;&otilde;es."
-    }
-  }
+
   document.getElementById("funcoes").innerHTML = funcoes
   document.getElementById("timer").innerHTML = text
 }, 1000)
